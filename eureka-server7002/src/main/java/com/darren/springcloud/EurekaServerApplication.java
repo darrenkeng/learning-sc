d29c0153ce0fbe04e72fa6f87e353a97ffdba893
darren <darrenkengg@gmail.com>
2021-02-22 17:02:37 +0800

    eureka集群

--- eureka-server7002/src/main/java/com/darren/springcloud/EurekaServerApplication.java
+++ eureka-server7002/src/main/java/com/darren/springcloud/EurekaServerApplication.java
@@ -2,18 +2,18 @@ package com.darren.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @description:
- * @author: Darren Keng
+ * @author: darren
  * @company: wonhigh
  * @create: 2021-02-22
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentProviderApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentProviderApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
--- order-consumer80/src/main/java/com/darren/springcloud/OrderConsumerApplication.java
+++ order-consumer80/src/main/java/com/darren/springcloud/OrderConsumerApplication.java
@@ -2,6 +2,7 @@ package com.darren.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description:
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2021-02-22
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderConsumerApplication.class, args);
