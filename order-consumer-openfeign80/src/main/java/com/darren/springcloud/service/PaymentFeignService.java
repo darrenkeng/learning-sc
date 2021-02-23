4145606c7200068d2c80e0beb66ef5eecc13f80f
darren <darrenkengg@gmail.com>
2021-02-23 12:08:52 +0800

    open feign consumer

---
+++ order-consumer-openfeign80/src/main/java/com/darren/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,23 @@
+package com.darren.springcloud.service;
+
+import com.darren.springcloud.entities.CommonResult;
+import com.darren.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @description:
+ * @author: darren
+ * @company: wonhigh
+ * @create: 2021-02-23
+ */
+@Component
+@FeignClient(value = "PAYMENT-PROVIDER")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+}
