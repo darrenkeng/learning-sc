a223900174ac79b689bcf50bd318c526adbe9430
darren <darrenkengg@gmail.com>
2021-02-28 20:22:03 +0800

    nacos 注册中心

---
+++ alibaba-order-consumer90/src/main/java/com/darren/springcloud/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.darren.springcloud.controller;
+
+import com.darren.springcloud.entities.CommonResult;
+import com.darren.springcloud.entities.Payment;
+import com.darren.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description:
+ * @author: darren
+ * @company: wonhigh
+ * @create: 2021-02-22
+ */
+@RestController
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "consumer/payment/nacos/{id}")
+    public String getPaymentById(@PathVariable("id") Integer id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+
+}
